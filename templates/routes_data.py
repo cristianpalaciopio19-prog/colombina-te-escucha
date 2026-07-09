0798d77e5c2c824c6ad4b9041ed5ac55b6ebdd90
cristianpalaciopio19-prog <cristianpalaciopio19@gmail.com>
2026-07-09 17:57:07 -0500

    Create routes_data.py

---
+++ templates/routes_data.py
@@ -0,0 +1,31 @@
+# Datos extraídos de "Programación_fija.xlsx" - hoja COLOMBINA FIJOS
+# 24 vehículos fijos de operación (buses propios/terceros/convenio)
+
+RUTAS = [
+    {"ruta": "T-2", "conductor": "Gustavo Adolfo Santa", "placa": "GUZ038", "municipio": "Tuluá"},
+    {"ruta": "T-3", "conductor": "Oscar Ladino Echeverry", "placa": "VAK305", "municipio": "Tuluá"},
+    {"ruta": "T-4", "conductor": "Carlos Murillo Ospina", "placa": "SIT809", "municipio": "Tuluá"},
+    {"ruta": "T-5", "conductor": "Didier Orlando Coy", "placa": "ESZ696", "municipio": "Tuluá"},
+    {"ruta": "T-6", "conductor": "Victor Alfonso Echeverry", "placa": "ETL439", "municipio": "Tuluá"},
+    {"ruta": "T-7", "conductor": "Jhon Pedro Gonzalez", "placa": "ESZ474", "municipio": "Tuluá"},
+    {"ruta": "T-8", "conductor": "Reynaldo Vasquez", "placa": "ESZ697", "municipio": "Tuluá"},
+    {"ruta": "T-9", "conductor": "Kevin Gonzalez Triviño", "placa": "TJY479", "municipio": "Tuluá"},
+    {"ruta": "A-1", "conductor": "Jorge Daniel Hernandez", "placa": "WHX092", "municipio": "Andalucía"},
+    {"ruta": "A-2", "conductor": "Francisco Javier Molina", "placa": "ESZ548", "municipio": "Andalucía"},
+    {"ruta": "A-3", "conductor": "Raul Andres Villada", "placa": "WHX093", "municipio": "Andalucía"},
+    {"ruta": "B-1", "conductor": "Leonardo Galvis Torres", "placa": "WHX088", "municipio": "Bugalagrande"},
+    {"ruta": "Z-2", "conductor": "Alvaro Cardona", "placa": "SZU346", "municipio": "Zarzal"},
+    {"ruta": "Z-4", "conductor": "Andres Felipe Vergara", "placa": "GIL764", "municipio": "Zarzal"},
+    {"ruta": "Z-5", "conductor": "Julio Cesar Torres", "placa": "WHX089", "municipio": "Zarzal"},
+    {"ruta": "Z-6", "conductor": "Erick Fernando Bedoya", "placa": "ZDA565", "municipio": "Zarzal"},
+    {"ruta": "Z-7", "conductor": "Miguel Anguel Piedrahita", "placa": "TTZ369", "municipio": "Zarzal"},
+    {"ruta": "Z-8", "conductor": "Dainer Marin", "placa": "STS028", "municipio": "Zarzal"},
+    {"ruta": "Z-9", "conductor": "Luis Felipe Arias", "placa": "ESZ549", "municipio": "Zarzal"},
+    {"ruta": "R-Z", "conductor": "Oscar Camilo Correcha", "placa": "WHX094", "municipio": "Zarzal"},
+    {"ruta": "R-CS", "conductor": "Carlos Alfonso Villegas", "placa": "TJY520", "municipio": "Zarzal"},
+    {"ruta": "R-1", "conductor": "Harold Giraldo Loaiza", "placa": "WHX095", "municipio": "Roldanillo"},
+    {"ruta": "R-2", "conductor": "Jhon James Castaño", "placa": "SIT798", "municipio": "Roldanillo"},
+    {"ruta": "R-3", "conductor": "Orbein Soto Bedoya", "placa": "WMV587", "municipio": "Roldanillo"},
+]
+
+TIPOS_PQR = ["Petición", "Queja", "Reclamo", "Sugerencia", "Felicitación"]
